e5aa04343622e90a8e50b61c3969144f9614d3b5
unknown <miguel2304.munoz@gmail.com>
2026-04-26 13:53:58 -0600

    Changed the way to get DATABASE_URL

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -4,7 +4,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("MYSQL_URL")
+DATABASE_URL = (
+    f"mysql+pymysql://{os.getenv('MYSQLUSER')}:"
+    f"{os.getenv('MYSQLPASSWORD')}@"
+    f"{os.getenv('MYSQLHOST')}:"
+    f"{os.getenv('MYSQLPORT')}/"
+    f"{os.getenv('MYSQLDATABASE')}?charset=utf8mb4"
+)
 if not DATABASE_URL:
     raise RuntimeError("DATABASE_URL is not set. Configure it in backend/.env")
 
